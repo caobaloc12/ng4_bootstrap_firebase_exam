d13ed3b724913e1356fe76d4d97a4f122a284c55
Loc Cao <loc.caoba@lazada.com>
2017-12-24 22:22:20 +0700

    first commit

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyDEfIHMpadiPX2qxklhXIL6r7DdpeGee_w',
+    authDomain: 'fir-proj-ng4.firebaseapp.com',
+    databaseURL: 'https://fir-proj-ng4.firebaseio.com',
+    projectId: 'fir-proj-ng4',
+    storageBucket: 'fir-proj-ng4.appspot.com',
+    messagingSenderId: '926368035144'
+  }
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,18 @@
+// The file contents for the current environment will overwrite these during build.
+// The build system defaults to the dev environment which uses `environment.ts`, but if you do
+// `ng build --env=prod` then `environment.prod.ts` will be used instead.
+// The list of which env maps to which file can be found in `.angular-cli.json`.
+
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyDEfIHMpadiPX2qxklhXIL6r7DdpeGee_w',
+    authDomain: 'fir-proj-ng4.firebaseapp.com',
+    databaseURL: 'https://fir-proj-ng4.firebaseio.com',
+    projectId: 'fir-proj-ng4',
+    storageBucket: 'fir-proj-ng4.appspot.com',
+    messagingSenderId: '926368035144'
+  }
+};
+
+
